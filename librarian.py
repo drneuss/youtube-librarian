2f1db8fe7e19916afedd24219491eb3edcd40bed
drneuss <109755882+drneuss@users.noreply.github.com>
2022-09-30 19:40:19 +0530

    Update librarian.py

--- librarian.py
+++ librarian.py
@@ -3,7 +3,7 @@ from os.path import isfile, join
 from subprocess import Popen
 import re
 def main():
-    p = Popen("run.bat", cwd=r".\") #run.bat
+    p = Popen("run.bat", cwd=r".\run.bat") #run.bat
     stdout, stderr = p.communicate()
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))] #outputfilelist
     currentdl = onlyfiles.open("r")
